7234f9ae91fd5677138c1e6c0c04b194aeedd028
Vasiliy <vasiliyasazonov@gmail.com>
2022-06-13 18:32:35 +0300

    add program week_day

--- Program.cs
+++ Program.cs
@@ -3,3 +3,15 @@
 //second branch - third_numb
 //third branch - week_day
 //fourth branch - numb_squared
+
+Console.Write("Input number week: ");
+int number_week = Convert.ToInt32(Console.ReadLine());
+
+if (number_week % 8 > 5)
+{
+    Console.Write(number_week + " - weekend");
+}
+else
+{
+    Console.Write(number_week + " - working day");
+}
