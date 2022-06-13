a90cfc82a2daa8b074a4cd68b3684ad4153d921d
Vasiliy <vasiliyasazonov@gmail.com>
2022-06-13 17:31:54 +0300

    add program second_number

--- Program.cs
+++ Program.cs
@@ -3,3 +3,15 @@
 //second branch - third_numb
 //third branch - week_day
 //fourth branch - numb_squared
+
+Console.Write("Input three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number = number * (-1);
+}
+number = number / 10;
+number = number % 10;
+ 
+Console.Write(number);
