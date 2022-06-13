4aaeb2b46fbfc1211121d29f0369ecbbb83f2d61
Vasiliy <vasiliyasazonov@gmail.com>
2022-06-13 18:01:23 +0300

    add .gitignore file new

--- Program.cs
+++ Program.cs
@@ -3,3 +3,24 @@
 //second branch - third_numb
 //third branch - week_day
 //fourth branch - numb_squared
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (true)
+{
+    if (number < 100)
+    {   
+        Console.Write("the third digit is missing \n");
+        Console.Write("Input a digit greater than 100: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        continue;
+    }
+    else if (number > 100)
+    {
+        break;
+    }
+}
+
+number = number % 10;
+Console.Write(number);
